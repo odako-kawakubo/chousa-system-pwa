bc105fb6711aac43f2770ebb9c9eb644fe7e9870
odako-kawakubo <odako.kawakubo@gmail.com>
2026-09-03 10:11:40 +0900

    v0.1.6.4C Service Worker更新

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,11 +1,11 @@
 /*
- * v0.1.6.4B Service Worker
+ * v0.1.6.4C Service Worker
  *
  * 責任は「アプリ本体を圏外でも起動できる状態に保つ」ことだけ。
  * 案件データ、IndexedDB、localStorage、未送信キュー、Firestore同期処理は扱わない。
  */
 
-const APP_CACHE = 'chousa-app-v0.1.6.4B';
+const APP_CACHE = 'chousa-app-v0.1.6.4C';
 const FIREBASE_SDK_CACHE = 'chousa-firebase-v12.1.0';
 const APP_CACHE_PREFIX = 'chousa-app-';
 const FIREBASE_SDK_PREFIX = 'https://www.gstatic.com/firebasejs/12.1.0/';
@@ -41,6 +41,7 @@ const APP_SHELL = [
   './js/projects/project-session.js',
   './js/projects/firestore-project-browser.js',
   './js/projects/project-transfer.js',
+  './js/demo/sample-session.js',
   './js/materials/simple-list.js',
   './config/app-config.js'
 ];
