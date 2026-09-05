67521a5ba9000ae88a324d256d3d621fc13cf9bd
odako-kawakubo <odako.kawakubo@gmail.com>
2026-09-06 02:54:09 +0900

    v0.1.6.5L バージョン更新

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -4,6 +4,6 @@
  */
 export const appConfig = {
   appName: '調査システムPWA',
-  version: '0.1.6.5K',
-  mode: 'photo-onedrive-sync-review'
+  version: '0.1.6.5L',
+  mode: 'photo-remote-display-review'
 };
