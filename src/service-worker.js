36ed4b7dd0f5c8584a9dc729c5fc3c9acf02bc47
odako-kawakubo <odako.kawakubo@gmail.com>
2026-09-24 17:49:03 +0900

    v0.1.8.10 サムネ即時表示版キャッシュ更新

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,10 @@
 /*
- * v0.1.8.9 Service Worker
+ * v0.1.8.10 Service Worker
  * 最新版優先 + 圏外時は直近キャッシュから起動する。
  */
-const APP_CACHE = 'chousa-app-v0.1.8.9-review';
+const APP_CACHE = 'chousa-app-v0.1.8.10-review';
 const FIREBASE_SDK_CACHE = 'chousa-firebase-v12.1.0';
-const OUTPUT_LIB_CACHE = 'chousa-output-libs-v0189-review';
+const OUTPUT_LIB_CACHE = 'chousa-output-libs-v01810-review';
 const APP_CACHE_PREFIX = 'chousa-app-';
 const FIREBASE_SDK_PREFIX = 'https://www.gstatic.com/firebasejs/12.1.0/';
 const OUTPUT_LIB_URLS = new Set([
